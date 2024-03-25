d3287aae83d2d60c12e2b927ac1fdb4df471f742
khadira1937 <okhadira@gmail.com>
2024-03-25 16:21:28 +0100

    tRPC setup (super handy backend tool)

--- petsandzoo/src/app/layout.tsx
+++ petsandzoo/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { cn } from "@/lib/utils";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn("relative h-full font-sans antialiased", inter.className)}>
+      <body 
+      className={cn(
+        "relative h-full font-sans antialiased", 
+        inter.className
+        )}>
         
         <main className="relative flex flex-col min-h-screen">
-          <Navbar />
-          <div className="flex-grow flex-1">{children}</div>
+          <Providers>
+            <Navbar />
+            <div className="flex-grow flex-1">
+              {children}
+            </div>
+          </Providers>
+          
         </main>
       </body>
     </html>
--- petsandzoo/src/server.ts
+++ petsandzoo/src/server.ts
@@ -1,9 +1,21 @@
 import express from "express"
 import { getPayloadClient } from "./get-payload"
 import { nextApp, nextHandler } from "./next-utils"
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { appRouter } from "./trpc"
+
+
 const app = express()
 
 const PORT = Number(process.env.PORT) || 3000
+
+const createContext = ({
+    req, res
+}: 
+trpcExpress.CreateExpressContextOptions) => ({
+    req,
+    res,
+})
 
 const start = async () => {
     const payload = await getPayloadClient({
@@ -14,6 +26,12 @@ const start = async () => {
             },
         },
     })
+
+    //kan kidwi 3la ycreer middlware kitsnet 3la express o next
+    app.use('/api/trpc',trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext
+    }))
 
     app.use((req, res) => nextHandler(req,res))
     
