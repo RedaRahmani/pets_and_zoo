1885441c3ac004bee24465911cc1a8b07cfcfe10
khadira1937 <okhadira@gmail.com>
2024-03-26 04:27:34 +0100

    Sending a sign-up verification email

--- petsandzoo/src/get-payload.ts
+++ petsandzoo/src/get-payload.ts
@@ -3,8 +3,20 @@ import path from "path"
 import payload from "payload"
 import type { InitOptions } from "payload/config"
 import { Payload } from "payload"
+import nodemailer from "nodemailer"
+
 dotenv.config({
     path: path.resolve(__dirname, "../.env")
+})
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.resend.com",
+    secure: true,
+    port: 465,
+    auth: {
+        user: "resend",
+        pass: process.env.RESEND_API_KEY ,
+    }
 })
 
 let cached = (global as any).payload
@@ -32,6 +44,12 @@ export const getPayloadClient = async ({
 
     if(!cached.promise){
         cached.promise = payload.init({
+            email: {
+                transport: transporter,
+                //hna khas ndiro domaine dyalna !!!
+                fromAddress: 'onboarding@resend.dev',
+                fromName: 'pets&zoo',
+            },
             secret: process.env.PAYLOAD_SECRET,
             local: initOptions?.express ? false : true ,
             ...(initOptions || {}),
